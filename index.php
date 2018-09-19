344e51efd083630f2801b0beb3884df099dc2ab8
Emma Grdina <emmagrdina@gmail.com>
2018-09-19 11:37:38 -0400

    updating the latest styles

--- index.php
+++ index.php
@@ -26,6 +26,11 @@
 		<p class="blurb">bsy is a platform to help women become mentors and find mentors in like-minded, or not so like-minded, women.</p> 
 	</div>
 
+	<div class='section audience'>
+		<h2>who it's for</h2>
+		<p class="blurb">bsy is for women looking for incredible mentors or those looking to find another. it’s also for those of us looking to pay it forward and become incredible mentors to other women.</p> 
+	</div>
+
 	<div class='section how'>
 		<h2>how it works</h2>
 		<ul id='one'><i class="fas fa-paper-plane"></i><h2>tell us a bit more about you</h2> 
@@ -39,17 +44,6 @@
 		</ul>
 	</div>
 
-	<div class='section audience'>
-		<h2>who it's for</h2>
-		<p class="blurb">bsy is for women looking for incredible mentors or those looking to find another. it’s also for those of us looking to pay it forward and become incredible mentors to other women.</p> 
-	</div>
-
-	<div class='section quote'>
-		<div class='all'><h2>who's bizzie?</h2>
-		<p>i'm bsy. you're bsy. we're all bsy.</p>
-		</div> 
-	</div>
-
 	<div class='section goal'>
 		<h2>our goal</h2>
 		<p class='blurb'>we hope it becomes a friendship where you discuss work, life, love, or all of the above. whatever you’re looking to get out of it, bsy will help you build a personal relationship through an individualized matching process.</p> 
@@ -59,7 +53,7 @@
 	</div>
 
 	<div class='section join'>
-		<h2>let's get started</h2>
+		<h2>join</h2>
 		<p class='blurb'>after you spend about 15 minutes telling us more about yourself, we will reach out about potentially pairing you with a mentor or mentee.</p> 
 		<div class='survey'>
 			<a href='http://emmagrdina.com/'>Let's get started<i class="fas fa-arrow-right"></i>
