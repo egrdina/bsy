8532ec9c8f3d3b10c13d37a92fa09ac5ea18856e
Emma Grdina <emmagrdina@gmail.com>
2018-09-24 11:55:29 -0400

    updating the contact us info

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
 <div class='footer'>
 	<ul id='copyright'><p>bsy © 2018</p></ul>
 	<ul id='slogan'><p>it's not business. it's bsy.</p></ul>
-	<ul id='contact'><p>contact us</p></ul>
+	<ul id='contact'><a href="mailto:livkirby@gmail.com"><p>contact us</p></a></ul>
 </div>
 	<?php wp_footer (); ?>
 </body>
