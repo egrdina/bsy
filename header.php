4b9f3820c6ddc0400367746590f886dc7b4acd72
Emma Grdina <emmagrdina@gmail.com>
2018-09-26 22:18:28 -0400

    bsy logo in the front page title

--- header.php
+++ header.php
@@ -21,7 +21,7 @@
 	</div>
 	<div class="top-nav">
     	<nav>
-			<div class='logo'><a href="<?php echo get_home_url(); ?>"><img src="<?php echo get_bloginfo('template_url') ?>/images/logo.png"/></a>
+			<div class='logo'><a href="<?php echo get_home_url(); ?>"><img alt="bsy" src="<?php echo get_bloginfo('template_url') ?>/images/logo.png"/></a>
 			</div>
 			<div class='menu-items'><ul><?php wp_nav_menu( array(' menu' => 'Main nav')); ?></ul>
 			</div>
@@ -30,7 +30,13 @@
 
 	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 	    <div class='section intro-blurb'>
-			<h1><?php echo the_title() ?></h1>
+			<h1>
+				<?php if (is_front_page()) { ?>
+					welcome to <img class='title-logo' alt="bsy" src="<?php echo get_bloginfo('template_url') ?>/images/logo.png"/>
+				<?php } else { ?>
+					<?php echo the_title() ?>
+				<?php } ?>
+			</h1>
 			<div class="blurb">
 				<?php echo the_content() ?>
 			</div>
